a2c08e21f9bf2983bcde3ba831167d65e36d7a02
jonathanadam <jonathan_adam@brown.edu>
2015-06-05 09:58:02 -0700

    Fixed syntax

--- proxy.js
+++ proxy.js
@@ -2,34 +2,38 @@ var http = require('http')
 var httpProxy = require('http-proxy')
 var config = require('./config.js')
 
-var port = 9000;
+var port = 9000
 
 var addresses = config.servers
 
 
-var proxy = httpProxy.createProxyServer();
-var count = 0;
-var server = http.createServer(function(req,res){
-    loadBalanceProxy(req,res);
-}).listen(port);
-
-currentServer = 0;
-function loadBalanceProxy(req, res){
-    var cur = currentServer%addresses.length;
-    currentServer++;
-    var target = addresses[cur];
-    proxy.web(req, res, {
-        target: target
-    }, function(e){
-    	//console.log("Server " + addresses[cur] + " has failed");
-    	loadBalanceProxy(req,res);
-    });
+var proxy = httpProxy.createProxyServer()
 
 
 
+var currentServer = 0
+function loadBalanceProxy(req, res)
+{
+	var cur = currentServer%addresses.length
+	currentServer += 1
+	var target = addresses[cur]
+	proxy.web(req, res,
+	{
+		target: target
+	}, function(err)
+	{
+		if (err)
+			throw err
+		loadBalanceProxy(req,res)
+	})
 }
 
+var server = http.createServer(function(req,res)
+{
+	loadBalanceProxy(req,res)
+})
 
+server.listen(port)
 
-console.log("listening at "+port);
+console.log("listening at "+port)
 
