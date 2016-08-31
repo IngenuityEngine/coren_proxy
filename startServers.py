cc7253020251bc96d7d7f22a991b094a60bbc104
Grant Miller <blented@gmail.com>
2016-08-31 10:05:56 -0700

    Revert "keep servers running for fun and profit"
    This reverts commit c574ba41fb609db7a2c75340363fe1a1dcc31399.

--- startServers.py
+++ startServers.py
@@ -2,32 +2,23 @@
 import sys
 import time
 import subprocess
-import psutil
-
-def startServer(command):
-	if sys.platform.startswith('win'):
-		return psutil.Popen(command, creationflags=subprocess.CREATE_NEW_CONSOLE)
-	else:
-		linuxCommand = 'xterm -hold -e "%s"' % command
-		return psutil.Popen(linuxCommand, shell=True)
 
 def main(baseCommand, startingPort, count):
-
-	servers = {}
+	procs = []
 	for i in range(1,count + 1):
-
 		command = baseCommand + ' ' + str(startingPort + i)
-		servers[i] = {
-			'command': command,
-			'process': startServer(command),
-		}
+		if sys.platform.startswith('win'):
+			process = subprocess.Popen(command, creationflags=subprocess.CREATE_NEW_CONSOLE)
+		else:
+			linuxCommand = 'xterm -hold -e "%s"' % command
+			process = subprocess.Popen(linuxCommand, shell=True)
+		procs.append(process)
 		time.sleep(3)
 
-	while True:
-		for i, server in servers.iteritems():
-			if not server['process'].is_running():
-				servers[i]['process'] = startServer(servers[i]['command'])
-
+	try:
+		input('Enter to exit from Python script...')
+	except:
+		pass
 
 
 if __name__ == '__main__':
