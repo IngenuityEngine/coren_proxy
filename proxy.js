1f1f07aadb9518abf21039f3fb930587c047c95e
Grant Miller <blented@gmail.com>
2017-04-07 20:19:03 -0700

    proxy updates otw to worker stuff

--- proxy.js
+++ proxy.js
@@ -2,9 +2,22 @@ var _ = require('lodash')
 var http = require('http')
 var args = require('optimist').argv
 var httpProxy = require('http-proxy')
-var config = require('./config/default.js')
+var cOS = require('commonos')
+var arkUtil = require('arkutil')
 
-config = _.merge(config, args)
+var userHome = cOS.getUserHome()
+console.log('home directory:', userHome)
+
+var config = require('./config/default.js')
+var userConfig = {}
+var userConfigPath = userHome + 'config/proxy.js'
+try {
+	userConfig = require(userConfigPath)
+} catch (err) {
+	console.log('could not find user config:', userConfigPath)
+}
+
+config = _.merge(config, userConfig, args)
 console.log('Config:')
 console.log(config)
 
@@ -40,12 +53,73 @@ function loadBalanceProxy(request, response)
 
 var server = http.createServer(function(request, response)
 {
-	// console.log(request.headers)
 	_.each(request.headers, function(val, key)
 	{
 		request.headers[key] = val.replace(headerRegex, '')
 	})
-	loadBalanceProxy(request, response)
+	console.log('request.url:', request.url)
+
+	if (request.url == '/_proxy/addServer' && request.method == 'POST')
+	{
+		console.log('Adding server')
+		var body = ''
+
+		request.on('data', function (data) {
+			body += data
+
+			// Too much POST data, kill the connection!
+			// 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+			if (body.length > 1e6)
+				request.connection.destroy()
+		})
+
+		request.on('end', function ()
+		{
+			var data = arkUtil.parseJSON(body)
+			console.log(data)
+
+			var err
+			if (!data ||
+				!_.isObject(data) ||
+				!data.url ||
+				!data.secret)
+			{
+				err = 'Invalid request, need url and secret'
+				console.log(err)
+				response.writeHead(500, {'Content-Type': 'text/html'})
+				response.write(err)
+				response.end()
+				return
+			}
+			if (data.secret != config.secret)
+			{
+				err = 'Incorrect proxy secret'
+				console.log(err)
+				response.writeHead(500, {'Content-Type': 'text/html'})
+				response.write(err)
+				response.end()
+				return
+			}
+
+			var responseData = {registered: 1}
+
+			response.writeHead(200, {'Content-Type': 'application/json'})
+			response.write(JSON.stringify(responseData))
+			response.end()
+			return
+		})
+	}
+	else if (request.url == '/_proxy/status')
+	{
+		var data = {'tacos': true}
+		response.writeHead(200, {'Content-Type': 'application/json'})
+		response.write(JSON.stringify(data))
+		response.end()
+	}
+	else
+	{
+		loadBalanceProxy(request, response)
+	}
 })
 
 server.listen(config.port)
